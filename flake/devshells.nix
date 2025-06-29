47118fdee378784d37a41085d4ce2459604b7c9f
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-29 15:37:24 +0530

    feat(nix): configure git-hooks and devshell
    - Adds git-hooks configuration to .gitignore
    - Configures pre-commit hooks in flake/checks.nix
    - Adds devshell configuration in flake/devshells.nix

--- flake/devshells.nix
+++ flake/devshells.nix
@@ -5,16 +5,32 @@
 }: {
   imports = [inputs.devshell.flakeModule];
 
-  perSystem = {pkgs, ...}: {
+  perSystem = {
+    pkgs,
+    config,
+    ...
+  }: {
     devshells.default = {
-      packages = lib.attrsets.attrValues {
-        inherit
-          (pkgs)
-          vscode-langservers-extracted
-          emmet-language-server
-          typescript-language-server
-          ;
-        inherit (pkgs.nodePackages) prettier;
+      devshell = rec {
+        name = "ProfessionalStay-Site";
+        motd = "{202}Welcome to {91}${name} {202}devshell!{reset} \n $(menu)";
+        startup = {
+          git-hooks.text = ''
+            ${config.pre-commit.installationScript}
+          '';
+        };
+        packages = lib.attrsets.attrValues {
+          inherit
+            (pkgs)
+            ### LSPs & Formatters
+            nixd
+            alejandra
+            vscode-langservers-extracted
+            emmet-language-server
+            typescript-language-server
+            ;
+          inherit (pkgs.nodePackages) prettier;
+        };
       };
     };
   };
